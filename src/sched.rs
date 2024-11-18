e37177ffcbf0b380c742ec270f82903575a0bc00
minhuw <wangmh15@gmail.com>
2024-11-18 16:28:21 +0000

    fix: trim \n at the end of comm

--- src/sched.rs
+++ src/sched.rs
@@ -86,7 +86,7 @@ impl TryFrom<u32> for Process {
             Entry::Occupied(o) => o.into_mut().clone(),
             Entry::Vacant(v) => v
                 .insert({
-                    if let Ok(name) = std::fs::read_to_string(format!("/proc/{}/comm", pid)) {
+                    if let Ok(name) = std::fs::read_to_string(format!("/proc/{}/comm", pid).trim_end()) {
                         name
                     } else {
                         "unknown".to_string()
